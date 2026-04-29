c506bf67b82f95285077c339296d9da8fcdd820e
ziyad-21 <163648448+ziyad-21@users.noreply.github.com>
2026-04-29 19:14:41 +0300

    Add files via upload

--- منصه-مشاريع-النيجر33/server.ts
+++ منصه-مشاريع-النيجر33/server.ts
@@ -337,7 +337,7 @@ async function startServer() {
   } else {
     const distPath = path.join(process.cwd(), 'dist');
     app.use(express.static(distPath));
-    app.get('*', (req, res) => {
+    app.get(/(.*)/, (req, res) => {
       res.sendFile(path.join(distPath, 'index.html'));
     });
   }
